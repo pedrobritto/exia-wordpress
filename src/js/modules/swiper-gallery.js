762edfe1287980be23677ec6f7d8750309da816f
Pedro Britto <pedroivobritto@gmail.com>
2019-06-06 14:14:56 -0300

    Updates swiper-gallery
    It can also receive strings as input instead
    of only HTMLElement

--- src/js/modules/swiper-gallery.js
+++ src/js/modules/swiper-gallery.js
@@ -1,3 +1,7 @@
+import { Swiper, Navigation, Pagination } from "swiper/dist/js/swiper.esm.js";
+
+Swiper.use([Navigation, Pagination]);
+
 /**
  * Navega até o índice de um swiper-slide correspondente
  * ao índice de um elemento clicado a partir de uma galeria fora do swiper.
@@ -9,16 +13,24 @@
  * @param {String} slideTriggerSelector
  *    Elemento que, quando clicado, navegará até o elemento
  *    correspondente no slider.
- * @param {Any} swiperConfig Objeto de configuração do Swiper.
+ * @param {Any} swiperConfig
+ *    Objeto de configuração do Swiper.
+ *
  * @returns {Void}
  */
 const swiperGallery = (swiperSelector, slideTriggerSelector, swiperConfig) => {
-    if (!document.querySelector(swiperSelector)) return false;
+    if (typeof swiperSelector === "string") {
+        if (!document.querySelector(swiperSelector)) return false;
+    }
 
     const slider = new Swiper(swiperSelector, swiperConfig);
 
     // Define z-index a partir de variável global definida em _z.scss
-    document.querySelector(swiperSelector).style.zIndex = "var(--z-index-modal-box)";
+    if (typeof swiperSelector === "string") {
+        document.querySelector(swiperSelector).style.zIndex = "var(--z-index-modal-box)";
+    } else {
+        swiperSelector.style.zIndex = "var(--z-index-modal-box)";
+    }
 
     /**
      * Navega slide swiperSelector até o índice do elemento clicado.
@@ -29,7 +41,13 @@ const swiperGallery = (swiperSelector, slideTriggerSelector, swiperConfig) => {
      *    clicado aqui.
      */
     const openSlideFrom = el => {
-        const galleryItems = document.querySelectorAll(el);
+        let galleryItems;
+
+        if (typeof el === "string") {
+            galleryItems = document.querySelectorAll(el);
+        } else {
+            galleryItems = el;
+        }
 
         Array.from(galleryItems).map((item, index) => {
             item.addEventListener("click", e => {
@@ -42,15 +60,4 @@ const swiperGallery = (swiperSelector, slideTriggerSelector, swiperConfig) => {
     openSlideFrom(slideTriggerSelector);
 };
 
-swiperGallery(".swiper-container.swiper-alugue", ".has-modal .gallery__item", {
-    slidesPerView: 1,
-    centeredSlides: true,
-    spaceBetween: 50,
-    keyboard: {
-        enabled: true,
-    },
-    navigation: {
-        nextEl: ".swiper-container.project-slider .button-next",
-        prevEl: ".swiper-container.project-slider .button-prev",
-    },
-});
+export default swiperGallery;
